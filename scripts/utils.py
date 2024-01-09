cfca10de09856f7903abad805a6af19e7c366d02
Samuel Svensson <samme.s90@gmail.com>
2024-01-09 23:37:26 +0100

    Add utility function to handle loading of images

---
+++ scripts/utils.py
@@ -0,0 +1,13 @@
+import pygame
+
+BASE_IMG_PATH = 'data/images/'
+
+
+def load_image(path):
+    # Using .convert() converts the internal representation of the image to
+    # make it more efficient for rendering.
+    img = pygame.image.load(BASE_IMG_PATH + path).convert()
+    # The black background will be rendered as transparent,
+    # i.e. one specfic color should be rendered transparent.
+    img.set_colorkey((0, 0, 0))
+    return img
