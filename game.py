73dc8a0e05b16d33cbedd8be98705df795c0c8de
Samuel Svensson <samme.s90@gmail.com>
2024-01-10 21:28:56 +0100

    Update and refactor the Game accordingly

--- game.py
+++ game.py
@@ -3,8 +3,8 @@ import sys
 import pygame
 
 from scripts.entities import PhysicsEntity
-from scripts.utils import load_image, load_images
 from scripts.tilemap import Tilemap
+from scripts.utils import load_image, load_images
 
 
 class Game:
@@ -35,35 +35,47 @@ class Game:
 
     def run(self):
         while True:
-            self.display.fill((14, 219, 248))
-
-            self.tilemap.render(self.display)
-
-            self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                # Keys should be arrow keys instead and preferably x and z for
-                # other actions. This is more universal for different keyboard
-                # layouts, but for my sake I'll use WASD.
-                if event.type == pygame.KEYDOWN:
-                    if event.key in (pygame.K_a, pygame.K_LEFT):
-                        self.movement[0] = True
-                    if event.key in (pygame.K_d, pygame.K_RIGHT):
-                        self.movement[1] = True
-                if event.type == pygame.KEYUP:
-                    if event.key in (pygame.K_a, pygame.K_LEFT):
-                        self.movement[0] = False
-                    if event.key in (pygame.K_d, pygame.K_RIGHT):
-                        self.movement[1] = False
+            self.clear()
+            self.handle_tilemap()
+            self.handle_player()
+            self.handle_events()
 
             self.screen.blit(pygame.transform.scale(
                 self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60)
+
+    def clear(self):
+        self.display.fill((14, 219, 248))
+
+    def handle_tilemap(self):
+        self.tilemap.render(self.display)
+
+    def handle_player(self):
+        self.player.update(self.tilemap,
+                           (self.movement[1] - self.movement[0], 0))
+        self.player.render(self.display)
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            # Keys should be arrow keys instead and preferably x and z for
+            # other actions. This is more universal for different keyboard
+            # layouts, but for my sake I'll use WASD.
+            if event.type == pygame.KEYDOWN:
+                if event.key in (pygame.K_a, pygame.K_LEFT):
+                    self.movement[0] = True
+                if event.key in (pygame.K_d, pygame.K_RIGHT):
+                    self.movement[1] = True
+                if event.key in (pygame.K_SPACE, pygame.K_UP):
+                    self.player.velocity[1] = -3
+            if event.type == pygame.KEYUP:
+                if event.key in (pygame.K_a, pygame.K_LEFT):
+                    self.movement[0] = False
+                if event.key in (pygame.K_d, pygame.K_RIGHT):
+                    self.movement[1] = False
 
 
 Game().run()
