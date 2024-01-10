a49e5855de859750a0acd2e32d641b66701e5ca0
Samuel Svensson <samme.s90@gmail.com>
2024-01-10 19:35:21 +0100

    Add function to load multiple images

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,5 @@
+import os
+
 import pygame
 
 BASE_IMG_PATH = 'data/images/'
@@ -8,6 +10,15 @@ def load_image(path):
     # make it more efficient for rendering.
     img = pygame.image.load(BASE_IMG_PATH + path).convert()
     # The black background will be rendered as transparent,
-    # i.e. one specfic color should be rendered transparent.
+    # i.e. one specific color should be rendered transparent.
     img.set_colorkey((0, 0, 0))
     return img
+
+
+def load_images(path):
+    images = []
+    # os.listdir() may not work on Linux. Using sorted should make this
+    # consistent across all platforms.
+    for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
+        images.append(load_image(path + '/' + img_name))
+    return images
