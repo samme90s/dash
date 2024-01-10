853508641449251f53125e8ef30ba89a7e9f3572
Samuel Svensson <samme.s90@gmail.com>
2024-01-10 19:48:40 +0100

    Add gravity to PhysicsEntity

--- scripts/entities.py
+++ scripts/entities.py
@@ -12,6 +12,10 @@ class PhysicsEntity:
         frame_movement = (movement[0] + self.velocity[0],
                           movement[1] + self.velocity[1])
 
+        # Apply gravity, with a terminal velocity.
+        # Positive y is down (not like a cartesian plane from math).
+        self.velocity[1] = min(5, self.velocity[1] + 0.1)
+
         # Usually want to update each axis separately, as below:
         self.pos[0] += frame_movement[0]
         self.pos[1] += frame_movement[1]
