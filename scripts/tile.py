ec6e9f2c7b504afddc94c08d844f80083d5c9f2c
Samuel Svensson <samme.s90@gmail.com>
2024-01-10 20:39:06 +0100

    Remove types for Tile

--- scripts/tile.py
+++ scripts/tile.py
@@ -1,5 +1,5 @@
 class Tile:
-    def __init__(self, type: str, variant: int, pos: tuple):
+    def __init__(self, type, variant, pos):
         self.type = type
         self.variant = variant
         self.pos = pos
