36c0192452116c0e0054b18dd33fc4dec42e863f
Samuel Svensson <samme.s90@gmail.com>
2024-01-10 23:44:58 +0100

    Add handle for scroll upon player repositioning

--- game.py
+++ game.py
@@ -34,10 +34,12 @@ class Game:
         self.tilemap = Tilemap(self, tile_size=16)
 
         self.scroll = [0, 0]
+        self.render_scroll = [0, 0]
 
     def run(self):
         while True:
             self.clear()
+            self.handle_scroll()
             self.handle_tilemap()
             self.handle_player()
             self.handle_events()
@@ -50,13 +52,22 @@ class Game:
     def clear(self):
         self.display.fill((14, 219, 248))
 
+    def handle_scroll(self):
+        self.scroll[0] += (self.player.rect().centerx -
+                           self.display.get_width() / 2 -
+                           self.scroll[0]) / 30
+        self.scroll[1] += (self.player.rect().centery -
+                           self.display.get_height() / 2 -
+                           self.scroll[1]) / 30
+        self.render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
     def handle_tilemap(self):
-        self.tilemap.render(self.display, offset=self.scroll)
+        self.tilemap.render(self.display, offset=self.render_scroll)
 
     def handle_player(self):
         self.player.update(self.tilemap,
                            (self.movement[1] - self.movement[0], 0))
-        self.player.render(self.display, offset=self.scroll)
+        self.player.render(self.display, offset=self.render_scroll)
 
     def handle_events(self):
         for event in pygame.event.get():
