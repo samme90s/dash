d7e470749d7a71707efb29d1e9a66c9b1849be04
Samuel Svensson <samme.s90@gmail.com>
2024-01-15 21:54:43 +0100

    Use Particle in Game

--- game.py
+++ game.py
@@ -1,13 +1,20 @@
+import math
+import random
 import sys
 
 import pygame
 
-from scripts.assets import AssetLayer, Assets
+from scripts.assets import AssetAnim, AssetLayer, Assets, AssetTile
 from scripts.clouds import Clouds
 from scripts.entities import Player
+from scripts.particle import Particle
 from scripts.tilemap import Tilemap
 from scripts.utils import Key
 from scripts.vector2 import Vector2
+
+# The number decides how often something spawns.
+# >number = less spawns
+SPAWN_RATE = 40_960
 
 
 class Game:
@@ -33,16 +40,17 @@ class Game:
                 lambda: self.player.jump()))
 
         self.assets = Assets()
-
         self.clouds = Clouds(self.assets.get_layers(AssetLayer.CLOUD), count=16)
-
         self.player = Player(self, Vector2((50, 50)), (8, 15))
-
         self.tilemap = Tilemap(self, tile_size=16)
-        try:
-            self.tilemap.load('map.json')
-        except FileNotFoundError:
-            pass
+        self.tilemap.load('map.json')
+
+        self.particles = []
+        self.spawns = []
+        for tree in self.tilemap.extract(
+                [(AssetTile.LARGE_DECOR, 2)], keep=True):
+            self.spawns.append(
+                pygame.Rect(*tree.pos.add((4, 4)), 23, 13))
 
         self.scroll = [0, 0]
         self.render_scroll = [0, 0]
@@ -55,7 +63,10 @@ class Game:
             self._handle_clouds()
             self._handle_tilemap()
             self._handle_player()
+            self._handle_spawns()
+            self._handle_particles()
             self._handle_events()
+            self.__dev()
             self._handle_screen()
 
     def _clear(self):
@@ -80,8 +91,35 @@ class Game:
 
     def _handle_player(self):
         self.player.update(self.tilemap,
-                           (self.movement[1] - self.movement[0], 0))
+                           (self.movement[1] - self.movement[0],
+                            0))
         self.player.render(self.display, offset=self.render_scroll)
+
+    def _handle_spawns(self):
+        for rect in self.spawns:
+            if random.random() * SPAWN_RATE < rect.width * rect.height:
+                pos = (Vector2((rect.x, rect.y))
+                       .add((random.random() * rect.width,
+                             random.random() * rect.height)))
+                self.particles.append(
+                    Particle(self,
+                             AssetAnim.PARTICLE_LEAF,
+                             pos,
+                             velocity=[-0.1, 0.3],
+                             frame=random.randint(0, 20)))
+
+    def _handle_particles(self):
+        for particle in self.particles.copy():
+            kill = particle.update()
+            if particle.asset == AssetAnim.PARTICLE_LEAF:
+                # Makes the leaf float left and right over time.
+                # 0.035 reduces the speed of the sine curve loop.
+                # 0.3 reduces the amplitude of the sine curve.
+                particle.pos.x += (math.sin(particle.animation.frame * 0.035) *
+                                   0.3)
+            particle.render(self.display, offset=self.render_scroll)
+            if kill:
+                self.particles.remove(particle)
 
     def _handle_events(self):
         for event in pygame.event.get():
@@ -96,6 +134,14 @@ class Game:
 
     def _set_movement(self, index, bool):
         self.movement[index] = bool
+
+    def __dev(self):
+        for rect in self.spawns:
+            adjusted_rect = pygame.Rect(rect.x - self.render_scroll[0],
+                                        rect.y - self.render_scroll[1],
+                                        rect.width,
+                                        rect.height)
+            pygame.draw.rect(self.display, (255, 0, 0), adjusted_rect, 1)
 
     def _handle_screen(self):
         self.screen.blit(
