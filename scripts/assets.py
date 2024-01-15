0f7b9f140408899ef840a9b947544cd0ca6b512b
Samuel Svensson <samme.s90@gmail.com>
2024-01-15 19:09:54 +0100

    Update assets classes

--- scripts/assets.py
+++ scripts/assets.py
@@ -4,48 +4,61 @@ from scripts.utils import Animation, load_images
 
 
 class AssetAnim(Enum):
-    PLAYER_IDLE = 'player/idle'
-    PLAYER_JUMP = 'player/jump'
-    PLAYER_RUN = 'player/run'
-    PLAYER_SLIDE = 'player/slide'
-    PLAYER_WALL_SLIDE = 'player/wall_slide'
+    PARTICLE_LEAF = 'particles/leaf'
+    PLAYER_IDLE = 'entities/player/idle'
+    PLAYER_JUMP = 'entities/player/jump'
+    PLAYER_RUN = 'entities/player/run'
+    PLAYER_SLIDE = 'entities/player/slide'
+    PLAYER_WALL_SLIDE = 'entities/player/wall_slide'
 
 
 class AssetLayer(Enum):
-    BACKGROUND = 'background'
-    CLOUD = 'cloud'
+    BACKGROUND = 'layers/backgrounds'
+    CLOUD = 'layers/clouds'
 
 
 class AssetTile(Enum):
-    DECOR = 'decor'
-    GRASS = 'grass'
-    LARGE_DECOR = 'large_decor'
-    STONE = 'stone'
+    DECOR = 'tiles/decor'
+    GRASS = 'tiles/grass'
+    LARGE_DECOR = 'tiles/large_decor'
+    STONE = 'tiles/stone'
 
 
 class Assets:
     def __init__(self):
         self.anims = {
-            'player/idle': Animation(
-                load_images('entities/player/idle'), img_dur=6),
-            'player/jump': Animation(
-                load_images('entities/player/jump')),
-            'player/run': Animation(
-                load_images('entities/player/run'), img_dur=4),
-            'player/slide': Animation(
-                load_images('entities/player/slide')),
-            'player/wall_slide': Animation(
-                load_images('entities/player/wall_slide'))
+            AssetAnim.PARTICLE_LEAF.value: Animation(
+                load_images(AssetAnim.PARTICLE_LEAF.value),
+                img_dur=20,
+                loop=False),
+            AssetAnim.PLAYER_IDLE.value: Animation(
+                load_images(AssetAnim.PLAYER_IDLE.value),
+                img_dur=6),
+            AssetAnim.PLAYER_JUMP.value: Animation(
+                load_images(AssetAnim.PLAYER_JUMP.value)),
+            AssetAnim.PLAYER_RUN.value: Animation(
+                load_images(AssetAnim.PLAYER_RUN.value),
+                img_dur=4),
+            AssetAnim.PLAYER_SLIDE.value: Animation(
+                load_images(AssetAnim.PLAYER_SLIDE.value)),
+            AssetAnim.PLAYER_WALL_SLIDE.value: Animation(
+                load_images(AssetAnim.PLAYER_WALL_SLIDE.value))
         }
         self.layers = {
-            'background': load_images('layers/backgrounds'),
-            'cloud': load_images('layers/clouds')
+            AssetLayer.BACKGROUND.value: load_images(
+                AssetLayer.BACKGROUND.value),
+            AssetLayer.CLOUD.value: load_images(
+                AssetLayer.CLOUD.value)
         }
         self.tiles = {
-            'decor': load_images('tiles/decor'),
-            'grass': load_images('tiles/grass'),
-            'large_decor': load_images('tiles/large_decor'),
-            'stone': load_images('tiles/stone')
+            AssetTile.DECOR.value: load_images(
+                AssetTile.DECOR.value),
+            AssetTile.GRASS.value: load_images(
+                AssetTile.GRASS.value),
+            AssetTile.LARGE_DECOR.value: load_images(
+                AssetTile.LARGE_DECOR.value),
+            AssetTile.STONE.value: load_images(
+                AssetTile.STONE.value)
         }
 
     def get_anim(self, type: AssetAnim):
